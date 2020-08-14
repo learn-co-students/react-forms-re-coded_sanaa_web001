41940768445c773512bc14b29455980c55e59d7a
Emad Alhattami <emad774373827@gmail.com>
2020-08-14 13:50:15 +0000

    Done.

---
+++ build/precache-manifest.985269cf4a4671e3f5aa20f7db2d2ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01862d57363b4b18f754f6c4c032efc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "927fbfa44be98a9afa1c",
+    "url": "/static/js/2.97b4407a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.97b4407a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ef437b873024bce06fb",
+    "url": "/static/js/main.3bb5fe61.chunk.js"
+  },
+  {
+    "revision": "b6b7d9f341d802461e6f",
+    "url": "/static/js/runtime-main.b2d6c529.js"
+  }
+]);
