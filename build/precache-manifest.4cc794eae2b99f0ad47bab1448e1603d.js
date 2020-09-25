d77b29fffef2a9aec75ef14854a0e234aa60143a
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-25 05:48:14 +0000

    Done.

---
+++ build/precache-manifest.4cc794eae2b99f0ad47bab1448e1603d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef2c9fe8dee46f4c5077c23f62fd2ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15fb645d9bd6df9918c4",
+    "url": "/static/js/2.7ac62fa9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7ac62fa9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12e7ae4d577efb4ecb49",
+    "url": "/static/js/main.530dfef5.chunk.js"
+  },
+  {
+    "revision": "b6b7d9f341d802461e6f",
+    "url": "/static/js/runtime-main.b2d6c529.js"
+  }
+]);
